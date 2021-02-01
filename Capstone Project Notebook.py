54e4e065f3cc8a7a3cff20d887eb8d20539c584d
Sand-Salamander <73651418+Sand-Salamander@users.noreply.github.com>
2021-01-31 16:51:31 -0800

    Add files via upload

---
+++ Capstone Project Notebook.py
@@ -0,0 +1,21 @@
+#!/usr/bin/env python
+# coding: utf-8
+
+# In[ ]:
+
+
+#a notebook for solving the project capstone
+
+
+# In[ ]:
+
+
+import pandas as pd
+import numpy as np
+
+
+# In[ ]:
+
+
+print("Hello Capstone Project Course!")
+
